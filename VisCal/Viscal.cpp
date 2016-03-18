a0b6f9d5311aedb5b211843462f47c8ca0cab735
hksuh <hksuh@kaist.ac.kr>
2016-03-18 16:53:08 +0900

    First Commit

---
+++ VisCal/Viscal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "matrix.h"
+
+using namespace std;
+
+int main(){
+	int a;
+	cout << "HEloow Wolrd;";
+	cin >> a;
+	cout << "you t" << endl;
+	return 0;
+}
---
+++ VisCal/matrix.h
@@ -0,0 +1,7 @@
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+typedef vector<double> mat1;
