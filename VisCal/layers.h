5fb866d99d059b97ebd736e3c4e33210e10ce047
hksuh <hksuh@kaist.ac.kr>
2016-03-19 17:46:28 +0900

    Add Comments and revise (indiv, layers, matrix)

--- VisCal/layers.h
+++ VisCal/layers.h
@@ -1,32 +1,46 @@
 #include "matrix.h"
 
-class calLayer1:public mat1{
+// matrix 자료형을 이용한 계산 layer상의 각부분 구현 
+//// 1. 계산흐름은 크게 두부분으로 구분된다.
+//////(1) kernel image convolution (unsupervised task-independent learning). 자유도를 줄이고 인지영역을 확장하는데에 초점이 있음.
+//////(2) fully connected neural layer (supervised task-dependent learning). Trainging example을 이용 mutation을 통해 알고리즘 학습.
+//// 2. (1)에서는 2차원 이미지의 다채널 정보가 레이어 한 층을 구성하므로 한 레이어가 3차원 행렬에 해당한다. (1)의 마지막 에서는 하나의 채널이 1*1 matrix가 되므로
+//// 이를 (2)에서 넘겨받을때에는 1차원 matrix를 사용하며 따라서 connectivity는 2차원, threshold는 1차원 행렬의 형태가 된다.
+
+// 계산진행이 저장되는 layer이다. 차원에 따라 이름뒤의 숫자가 구분되며 channelLayer는 여러채널 레이어 묶음을 나타낸다. 따라서 (1)영역은 channelLayer2, (2)영역은 layer1이 사용된다. 필요한 레이어를 모두 묶어 totalLayer라 한다.
+//class layer1:public mat1{};
+//class layer2:public mat2{};
+//class channelLayer2 :public vector<layer2>{};
+typedef mat1 layer1;
+typedef mat2 layer2;
+typedef vector<layer2> channelLayer2;
+class totalLayer{
+	vector<channelLayer2> frontL;
+	vector<layer1> rearL;
 };
 
-class calLayer2:public mat2{
-};
-
-class calLayer{
-	vector<vector<calLayer2>> preL;
-	vector<calLayer1> postL;
-};
-
+/*connectivity : keyfunction : layer1 input을 받아 행렬곱하여 layer1계산결과를 target에 쓰는 것.*/
 class conn:public mat2{
 public:
-	void getNext(const calLayer1& _input, calLayer1 _target);
+	void getNext(const layer1& _input, layer1 _target);
 };
 
+/*kernel : keyfunction : layer2나 channelLayer2를 input으로 받아 계산결과를 layer2 target에 쓰는 것. */
 class krnl{
 public:
-	void getNext(const calLayer2& _input, calLayer2 _target);
+	void getNext(const layer2& _input, layer2 _target);
+	void getNext(const channelLayer2& _input, layer2 _target);
 };
 
+/*threshold : keyfunction : layer1 target의 데이터를 sigmoid 연산하여 수정하는것. */
 class thsd{
 public:
-	void getNext(calLayer1& _target);
+	void getNext(layer1& _target);
 };
 
+/*trainData : train할 input들과 알고있는 결과를 저장해둔다.*/
 struct trainData{
-	vector<mat2> input;
-	vector<mat1> expectedResult;
+	int numOfData;
+	[]channelLayer2 input;
+	mat1 expectedResult[];
 };
--- VisCal/matrix.h
+++ VisCal/matrix.h
@@ -4,28 +4,59 @@
 
 using namespace std;
 
-class mat1: public vector<double>{
+// vector 자료형 상속을 이용하여 다차원 행렬과 그에 필요한 연산구현
+//// 1. template을 이용하여 recursion으로 class를 정의하여 임의의 차원행렬에 대해 일반화 할수도 있지만
+//// 이 문제에서는 각각의 차원의 행렬이 쓰임이 구분되고 임의의 모든 차원이 아닌 1,2,3차원만 주로 사용되므로 차원일반화를 하지 않는다.  
+//// 2. 함수를 이용할때 input,target 변수들과 현재행렬의 길이가 맞게 주어진다 가정한다. (계산효율을 위해 검사문이나 예외처리를 따로 신경쓰지 않는다.)
+
+typedef double T;
+// 후에 double이 아닌 다른 자료형을 사용할 경우 typedef T의 자료형을 바꾸어주면 된다.
+// T 자료형의 사용연산들 (코드구현시 주석 추가)
+/* operator + */
+/* operator * */
+/* toStr (cout 출력문) */
+
+// 공통함수들
+/* add : 각각의 행렬원소들을 더한다.*/
+/* copy : 깊은복사를 수행한다. cppy constructor랑 operator=이랑 어떻게 사용해야될지 아직 모르겠음*/
+/* rand : 원소값들을 랜덤으로 생성한다.*/
+/* toStr : 원소정보들을 string으로 변환한다. file I/O와 show 함수에 사용된다.*/
+/* show : toStr을 이용하여 객체의 변수정보들을 출력한다*/
+
+class mat1 : public vector<T>{
 public:
-	mat1():vector<double>(){}
-	mat1(size_t _n,double _init) :vector<double>(_n,_init){}
+	mat1():vector<T>(){}
+	mat1(size_t _n,T _init):vector<T>(_n,_init){}
 
 	void add(const mat1& _a, const mat1& _b){}
 
-	void flatten(const mat2& _a){}
-
-	void flatten(const mat3& _a){}
-
-	void show(){}
+	/* flatten : 2차원이상의 행렬의 원소들을 일렬로 펼쳐서 1차원행렬(column vector)로 만든다.*/
+	void flatten();
+	
+	void add();
+	void copy();
+	void rand();
+	string toStr();
+	void show();
 };
+
+void mat1::show(){/*이런식으로 cpp파일에 함수 내용물 구현*/};
 
 class mat2 : public vector<mat1>{
 public:
 	mat2() :vector<mat1>(){}
 	mat2(size_t _n, mat1 _init) :vector<mat1>(_n, _init){}
 
-	void sigmoid(const mat2& _thsd){}
+	/*thsd를 이용하여 각각의 원소값에 sigmoid함수를 취하여 0~1사이 실수로 만든다.*/
+	void sigmoid(const mat2& _thsd);
 
-	void show(){}
+	/*input 열벡터에 현재행렬을 곱한 결과를 target 열벡터에 저장한다.*/
+	void product(const mat1& _input, mat1& _target);
+	
+	void add();
+	void copy();
+	string toStr();
+	void show();
 };
 
 class mat3{};
