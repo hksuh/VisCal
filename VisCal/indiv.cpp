c2d00f69d655202068949ab2af0ae66b576ecd7b
hksuh <hksuh@kaist.ac.kr>
2016-04-03 00:41:08 +0900

    debugginging

--- VisCal/indiv.cpp
+++ VisCal/indiv.cpp
@@ -447,7 +447,7 @@ void indiv::calScore(totalLayer& _layers, const trainData& _trainData, int dataN
 	}
 	for (int i = 0; i < depthF-1; i++){
 		for (int j = 0; j < size_frontL[i][0]; j++){ //channel
-			krnls[i][j].getNext(_layers.frontL[i], _layers.frontL[i+1][j]);
+			krnls[i+1][j].getNext(_layers.frontL[i], _layers.frontL[i+1][j]);
 		}
 	}
 	flatten(_layers.frontL[depthF-1],_layers.rearL[0]); //insert flatten function to make it mat1
